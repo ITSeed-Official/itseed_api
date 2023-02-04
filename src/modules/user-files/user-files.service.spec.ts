842579bcd1aa6eee5eb159b0f35db3a6eaf053d2
Alan Hsueh <alan.hsueh@shoplineapp.com>
2023-02-04 23:38:26 +0800

    feat(UserFilesService): add updateByUserId and isComplete unit tests

--- src/modules/user-files/user-files.service.spec.ts
+++ src/modules/user-files/user-files.service.spec.ts
@@ -3,14 +3,66 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { UserFileEntity } from "./entities/user-files.entity";
 import { UserFilesService } from "./user-files.service";
+import { UserFiles as UserFilesDto } from "../applications/dtos/update-application-payload.dto";
 
 describe("UserFilesService", () => {
   let service: UserFilesService;
   const awsUrl = "AWS_CLOUDFRONT";
   const userId = 1;
 
+  const userFilesIsCompleteTestCase = [
+    {
+      expected: false,
+      files: [
+        {
+          userId: 1,
+          type: "certification",
+          path: "certification file path",
+          name: "certification file name",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    },
+    {
+      expected: false,
+      files: [
+        {
+          userId: 1,
+          type: "resume",
+          path: "resume file path",
+          name: "resume file name",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    },
+    {
+      expected: true,
+      files: [
+        {
+          userId: 1,
+          type: "resume",
+          path: "resume file path",
+          name: "resume file name",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 1,
+          type: "certification",
+          path: "certification file path",
+          name: "certification file name",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    },
+  ];
+
   const mockUserFileRepo = {
     find: jest.fn(),
+    upsert: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -89,6 +141,81 @@ describe("UserFilesService", () => {
             path: `${awsUrl}certification file path`,
             name: "certification file name",
           },
+        });
+      });
+    });
+  });
+
+  describe("#updateByUserId", () => {
+    const userFilesDto: UserFilesDto = {
+      resume: {
+        path: "resume path",
+        name: "resume name",
+      },
+      certification: {
+        path: "certification path",
+        name: "certification name",
+      },
+    };
+
+    describe("update user files", () => {
+      it("should update user files successfully", async () => {
+        jest
+          .spyOn(mockUserFileRepo, "upsert")
+          .mockImplementation((updateData, options) => {
+            expect(updateData).toEqual([
+              {
+                type: "resume",
+                userId: userId,
+                name: "resume name",
+                path: "resume path",
+              },
+              {
+                type: "certification",
+                userId: userId,
+                name: "certification name",
+                path: "certification path",
+              },
+            ]);
+
+            expect(options).toEqual({
+              conflictPaths: ["userId", "type"],
+              skipUpdateIfNoValuesChanged: true,
+            });
+          });
+
+        await service.updateByUserId(userId, userFilesDto);
+      });
+    });
+
+    describe("repository update data failed", () => {
+      it("should throw exception", async () => {
+        jest.spyOn(mockUserFileRepo, "upsert").mockImplementation(() => {
+          throw new Error("update data failed");
+        });
+
+        try {
+          await service.updateByUserId(userId, userFilesDto);
+        } catch (error) {
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe("update data failed");
+        }
+      });
+    });
+  });
+
+  describe("#isComplete", () => {
+    describe("when resume is not uploaded", () => {
+      userFilesIsCompleteTestCase.forEach((testCase) => {
+        it("should return false", async () => {
+          jest
+            .spyOn(mockUserFileRepo, "find")
+            .mockImplementation((): UserFileEntity[] => {
+              return testCase.files;
+            });
+
+          const result = await service.isComplete(userId);
+          expect(result).toBe(testCase.expected);
         });
       });
     });
