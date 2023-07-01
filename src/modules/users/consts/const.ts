b1daff4ba00a7aaf6b11178ca3fdef90ee0c745b
Alan Hsueh <alan.hsueh@shoplineapp.com>
2023-07-01 13:02:47 +0800

    feat: adjust end time

--- src/modules/users/consts/const.ts
+++ src/modules/users/consts/const.ts
@@ -108,4 +108,4 @@ export const START_TIME =
   process.env.START_TIME || `${CURRENT_YEAR.toString()}-06-01T16:00:00.000Z`;
 // ISO 8601
 export const END_TIME =
-  process.env.END_TIME || `${CURRENT_YEAR.toString()}-07-01T15:59:59.999Z`;
+  process.env.END_TIME || `${CURRENT_YEAR.toString()}-07-04T15:59:59.999Z`;
