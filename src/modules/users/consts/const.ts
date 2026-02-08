52600c71dc63468465a22217909773e0783261dc
alansyue <b123105@gmail.com>
2026-02-09 00:26:00 +0800

    Update registration window constants

--- src/modules/users/consts/const.ts
+++ src/modules/users/consts/const.ts
@@ -105,7 +105,7 @@ export const USER_FILE_COMPLETION = 4;
 export const CURRENT_YEAR = new Date().getFullYear();
 // ISO 8601
 export const START_TIME =
-  process.env.START_TIME || `${CURRENT_YEAR.toString()}-05-25T16:00:00.000Z`;
+  process.env.START_TIME || "2026-01-25T16:00:00.000Z";
 // ISO 8601
 export const END_TIME =
-  process.env.END_TIME || `${CURRENT_YEAR.toString()}-07-08T15:59:59.999Z`;
+  process.env.END_TIME || "2026-01-28T15:59:59.999Z";
